290bfdc1b6ad6bf855cf8427d7618738dfb3cef1
nguyentiendat <nguyentiendat03091995@gmail.com>
2017-03-16 14:20:41 +0700

    dat ten ucDashboard

--- DemoMetroUI/ucDashboard.Designer.cs
+++ DemoMetroUI/ucDashboard.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.mtThem = new MetroFramework.Controls.MetroTile();
-            this.metroTile4 = new MetroFramework.Controls.MetroTile();
-            this.metroTile5 = new MetroFramework.Controls.MetroTile();
+            this.mtTheodoi = new MetroFramework.Controls.MetroTile();
+            this.mtYkien = new MetroFramework.Controls.MetroTile();
             this.metroTile7 = new MetroFramework.Controls.MetroTile();
-            this.metroTile9 = new MetroFramework.Controls.MetroTile();
-            this.metroTile10 = new MetroFramework.Controls.MetroTile();
-            this.metroTile1 = new MetroFramework.Controls.MetroTile();
-            this.metroTile2 = new MetroFramework.Controls.MetroTile();
-            this.metroTile6 = new MetroFramework.Controls.MetroTile();
-            this.metroTile8 = new MetroFramework.Controls.MetroTile();
+            this.mtImport = new MetroFramework.Controls.MetroTile();
+            this.mtBaocao = new MetroFramework.Controls.MetroTile();
+            this.mtDanhgia = new MetroFramework.Controls.MetroTile();
+            this.mtTimkiem = new MetroFramework.Controls.MetroTile();
+            this.mtLich = new MetroFramework.Controls.MetroTile();
+            this.mtLapphieudiem = new MetroFramework.Controls.MetroTile();
             this.SuspendLayout();
             // 
             // mtThem
@@ -52,26 +52,26 @@
             this.mtThem.UseSelectable = true;
             this.mtThem.Click += new System.EventHandler(this.metroTile3_Click);
             // 
-            // metroTile4
+            // mtTheodoi
             // 
-            this.metroTile4.ActiveControl = null;
-            this.metroTile4.Location = new System.Drawing.Point(3, 167);
-            this.metroTile4.Name = "metroTile4";
-            this.metroTile4.Size = new System.Drawing.Size(104, 60);
-            this.metroTile4.TabIndex = 0;
-            this.metroTile4.Text = "Theo Dõi";
-            this.metroTile4.UseSelectable = true;
+            this.mtTheodoi.ActiveControl = null;
+            this.mtTheodoi.Location = new System.Drawing.Point(3, 167);
+            this.mtTheodoi.Name = "mtTheodoi";
+            this.mtTheodoi.Size = new System.Drawing.Size(104, 60);
+            this.mtTheodoi.TabIndex = 0;
+            this.mtTheodoi.Text = "Theo Dõi";
+            this.mtTheodoi.UseSelectable = true;
             // 
-            // metroTile5
+            // mtYkien
             // 
-            this.metroTile5.ActiveControl = null;
-            this.metroTile5.Location = new System.Drawing.Point(113, 167);
-            this.metroTile5.Name = "metroTile5";
-            this.metroTile5.Size = new System.Drawing.Size(104, 192);
-            this.metroTile5.TabIndex = 0;
-            this.metroTile5.Text = "Ý Kiến SV";
-            this.metroTile5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.metroTile5.UseSelectable = true;
+            this.mtYkien.ActiveControl = null;
+            this.mtYkien.Location = new System.Drawing.Point(113, 167);
+            this.mtYkien.Name = "mtYkien";
+            this.mtYkien.Size = new System.Drawing.Size(104, 192);
+            this.mtYkien.TabIndex = 0;
+            this.mtYkien.Text = "Ý Kiến SV";
+            this.mtYkien.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.mtYkien.UseSelectable = true;
             // 
             // metroTile7
             // 
@@ -83,79 +83,79 @@
             this.metroTile7.Text = "Chức Năng Offline";
             this.metroTile7.UseSelectable = true;
             // 
-            // metroTile9
+            // mtImport
             // 
-            this.metroTile9.ActiveControl = null;
-            this.metroTile9.Location = new System.Drawing.Point(595, 263);
-            this.metroTile9.Name = "metroTile9";
-            this.metroTile9.Size = new System.Drawing.Size(104, 90);
-            this.metroTile9.TabIndex = 0;
-            this.metroTile9.Text = "Import\r\nExport";
-            this.metroTile9.UseSelectable = true;
+            this.mtImport.ActiveControl = null;
+            this.mtImport.Location = new System.Drawing.Point(595, 263);
+            this.mtImport.Name = "mtImport";
+            this.mtImport.Size = new System.Drawing.Size(104, 90);
+            this.mtImport.TabIndex = 0;
+            this.mtImport.Text = "Import\r\nExport";
+            this.mtImport.UseSelectable = true;
             // 
-            // metroTile10
+            // mtBaocao
             // 
-            this.metroTile10.ActiveControl = null;
-            this.metroTile10.Location = new System.Drawing.Point(485, 263);
-            this.metroTile10.Name = "metroTile10";
-            this.metroTile10.Size = new System.Drawing.Size(104, 90);
-            this.metroTile10.TabIndex = 0;
-            this.metroTile10.Text = "Báo Cáo \r\nThống Kê";
-            this.metroTile10.UseSelectable = true;
+            this.mtBaocao.ActiveControl = null;
+            this.mtBaocao.Location = new System.Drawing.Point(485, 263);
+            this.mtBaocao.Name = "mtBaocao";
+            this.mtBaocao.Size = new System.Drawing.Size(104, 90);
+            this.mtBaocao.TabIndex = 0;
+            this.mtBaocao.Text = "Báo Cáo \r\nThống Kê";
+            this.mtBaocao.UseSelectable = true;
             // 
-            // metroTile1
+            // mtDanhgia
             // 
-            this.metroTile1.ActiveControl = null;
-            this.metroTile1.Location = new System.Drawing.Point(3, 233);
-            this.metroTile1.Name = "metroTile1";
-            this.metroTile1.Size = new System.Drawing.Size(104, 60);
-            this.metroTile1.TabIndex = 0;
-            this.metroTile1.Text = "Đánh Giá";
-            this.metroTile1.UseSelectable = true;
+            this.mtDanhgia.ActiveControl = null;
+            this.mtDanhgia.Location = new System.Drawing.Point(3, 233);
+            this.mtDanhgia.Name = "mtDanhgia";
+            this.mtDanhgia.Size = new System.Drawing.Size(104, 60);
+            this.mtDanhgia.TabIndex = 0;
+            this.mtDanhgia.Text = "Đánh Giá";
+            this.mtDanhgia.UseSelectable = true;
             // 
-            // metroTile2
+            // mtTimkiem
             // 
-            this.metroTile2.ActiveControl = null;
-            this.metroTile2.Location = new System.Drawing.Point(3, 299);
-            this.metroTile2.Name = "metroTile2";
-            this.metroTile2.Size = new System.Drawing.Size(104, 60);
-            this.metroTile2.TabIndex = 0;
-            this.metroTile2.Text = "Tìm Kiếm";
-            this.metroTile2.UseSelectable = true;
+            this.mtTimkiem.ActiveControl = null;
+            this.mtTimkiem.Location = new System.Drawing.Point(3, 299);
+            this.mtTimkiem.Name = "mtTimkiem";
+            this.mtTimkiem.Size = new System.Drawing.Size(104, 60);
+            this.mtTimkiem.TabIndex = 0;
+            this.mtTimkiem.Text = "Tìm Kiếm";
+            this.mtTimkiem.UseSelectable = true;
             // 
-            // metroTile6
+            // mtLich
             // 
-            this.metroTile6.ActiveControl = null;
-            this.metroTile6.Location = new System.Drawing.Point(485, 47);
-            this.metroTile6.Name = "metroTile6";
-            this.metroTile6.Size = new System.Drawing.Size(214, 90);
-            this.metroTile6.TabIndex = 0;
-            this.metroTile6.Text = "Lịch";
-            this.metroTile6.UseSelectable = true;
+            this.mtLich.ActiveControl = null;
+            this.mtLich.Location = new System.Drawing.Point(485, 47);
+            this.mtLich.Name = "mtLich";
+            this.mtLich.Size = new System.Drawing.Size(214, 90);
+            this.mtLich.TabIndex = 0;
+            this.mtLich.Text = "Lịch";
+            this.mtLich.UseSelectable = true;
             // 
-            // metroTile8
+            // mtLapphieudiem
             // 
-            this.metroTile8.ActiveControl = null;
-            this.metroTile8.Location = new System.Drawing.Point(485, 167);
-            this.metroTile8.Name = "metroTile8";
-            this.metroTile8.Size = new System.Drawing.Size(214, 90);
-            this.metroTile8.TabIndex = 0;
-            this.metroTile8.Text = "Lập Phiếu Điểm";
-            this.metroTile8.UseSelectable = true;
+            this.mtLapphieudiem.ActiveControl = null;
+            this.mtLapphieudiem.Location = new System.Drawing.Point(485, 167);
+            this.mtLapphieudiem.Name = "mtLapphieudiem";
+            this.mtLapphieudiem.Size = new System.Drawing.Size(214, 90);
+            this.mtLapphieudiem.TabIndex = 0;
+            this.mtLapphieudiem.Text = "Lập Phiếu Điểm";
+            this.mtLapphieudiem.UseSelectable = true;
             // 
             // ucDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.metroTile9);
+            this.Controls.Add(this.mtImport);
             this.Controls.Add(this.mtThem);
-            this.Controls.Add(this.metroTile5);
-            this.Controls.Add(this.metroTile10);
-            this.Controls.Add(this.metroTile2);
-            this.Controls.Add(this.metroTile1);
-            this.Controls.Add(this.metroTile4);
-            this.Controls.Add(this.metroTile8);
-            this.Controls.Add(this.metroTile6);
+            this.Controls.Add(this.mtYkien);
+            this.Controls.Add(this.mtBaocao);
+            this.Controls.Add(this.mtTimkiem);
+            this.Controls.Add(this.mtDanhgia);
+            this.Controls.Add(this.mtTheodoi);
+            this.Controls.Add(this.mtLapphieudiem);
+            this.Controls.Add(this.mtLich);
             this.Controls.Add(this.metroTile7);
             this.Name = "ucDashboard";
             this.Size = new System.Drawing.Size(702, 404);
@@ -166,14 +166,14 @@
 
         #endregion
         private MetroFramework.Controls.MetroTile mtThem;
-        private MetroFramework.Controls.MetroTile metroTile4;
-        private MetroFramework.Controls.MetroTile metroTile5;
+        private MetroFramework.Controls.MetroTile mtTheodoi;
+        private MetroFramework.Controls.MetroTile mtYkien;
         private MetroFramework.Controls.MetroTile metroTile7;
-        private MetroFramework.Controls.MetroTile metroTile9;
-        private MetroFramework.Controls.MetroTile metroTile10;
-        private MetroFramework.Controls.MetroTile metroTile1;
-        private MetroFramework.Controls.MetroTile metroTile2;
-        private MetroFramework.Controls.MetroTile metroTile6;
-        private MetroFramework.Controls.MetroTile metroTile8;
+        private MetroFramework.Controls.MetroTile mtImport;
+        private MetroFramework.Controls.MetroTile mtBaocao;
+        private MetroFramework.Controls.MetroTile mtDanhgia;
+        private MetroFramework.Controls.MetroTile mtTimkiem;
+        private MetroFramework.Controls.MetroTile mtLich;
+        private MetroFramework.Controls.MetroTile mtLapphieudiem;
     }
 }
